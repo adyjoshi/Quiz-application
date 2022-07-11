f185f67dd16ee5c7fa1d82093dff9401e2bb0051
Aditi Joshi <38169015+adyjoshi@users.noreply.github.com>
2022-07-10 22:58:58 -0230

    created app.js

--- quiz-app/src/Form/SetupForm.jsx
+++ quiz-app/src/Form/SetupForm.jsx
@@ -8,8 +8,28 @@ const SetupForm =() =>{
                         <label htmlFor="amount">number of questions</label>
                         <input type="number" name="amount" id="amount" className="form-input"/>
                     </div>
+                    <div className="form-control">
+                        <label htmlFor="category">category</label>
+                        <select name="category" id="category" className="form-input">
+                            <option value="sports">sports</option>
+                            <option value="history">history</option>
+                            <option value="politics">politics</option>
+                        </select>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="difficulty">difficulty</label>
+                        <select name="difficulty" id="difficulty" className="form-input">
+                            <option value="easy">easy</option>
+                            <option value="medium">medium</option>
+                            <option value="difficult">difficult</option>
+                        </select>
+                    </div>
+                    <p className="error">Can't genearate questions, Please try again!</p>
+                    <button type="submit" className="submit-btn">start</button>
                 </form>
             </section>
         </main>
-    )
-}
+    );
+};
+
+export default SetupForm;
